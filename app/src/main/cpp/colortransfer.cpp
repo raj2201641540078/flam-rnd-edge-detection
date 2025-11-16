5e151c460b10fd20944bf0788bd0bb22f3435d4b
Raj <2k22.psit.csds.32072@gmail.com>
2025-11-16 12:14:30 +0530

    updated project

--- app/src/main/cpp/colortransfer.cpp
+++ app/src/main/cpp/colortransfer.cpp
@@ -39,9 +39,7 @@ Mat color_transfer(Mat &color_source,Mat &target,bool preserve_paper){
     if(preserve_paper){
         //使用论文提出的缩放因子来对标准差进行缩放
         tar_labChannels[0] = (tar_mean_std[1] / src_mean_std[1]) * tar_labChannels[0]; // l = (l_std_tar / l_std_src) * l
-        tar_labChannels[1] = (tar_mean_std[3] / src_mean_std[3]) * tar_labChannels[1];
-        tar_labChannels[2] = (tar_mean_std[5] / src_mean_std[5]) * tar_labChannels[2];
-    }else{
+      
         //使用论文提出的缩放因子的导数来对标准差进行缩放
         tar_labChannels[0] = (src_mean_std[1] / tar_mean_std[1] ) * tar_labChannels[0]; // l = (l_std_src / l_std_tar ) * l
         tar_labChannels[1] = (src_mean_std[3] / tar_mean_std[3] ) * tar_labChannels[1];
